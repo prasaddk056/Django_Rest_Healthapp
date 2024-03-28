9283dfd1dda7872186bf6566087d3963f32a396c
prasaddk056 <prasaddk056@gmail.com>
2024-03-28 13:47:40 +0530

    Add first api project to github

---
+++ healthapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Data
+from .serializer import DataSerializer
+# Create your views here.
+
+@api_view(["GET"])
+def getData(request):
+    app = Data.objects.all()
+    
+    serializer = DataSerializer(app , many = True)
+
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def postData(request):
+    serializer = DataSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
